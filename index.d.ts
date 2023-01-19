f416f6f9270a7cc5774075c26349e28faef26870
Carlos && Ponce <srtager555@gmail.com>
2023-01-19 00:08:10 -0600

    I added a new Interface to start the code

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,23 @@
+type SeekerWordType = Array<string | string[]>;
+
 interface WrappingLetters {
   text: string;
   classes: string;
   wrapping: unknown;
+}
+
+interface WLProps {
+  text?: string;
+  textOptions?: {
+    ClassToAdd?: string;
+    SelecClass?: {
+      wordToSearch: SeekerWordType;
+      classToAdd: string | string[];
+    };
+    SpecialWrapp?: {
+      wordToSearch: SeekerWordType;
+      structuretToAdd: HTMLElement;
+    };
+    PerWord?: boolean;
+  };
 }
