98ee2cf69d27f031d27d3a05be86ade6e88c6a85
Carlos && Ponce <srtager555@gmail.com>
2023-01-19 22:08:56 -0600

    I moved the base.class.js to the engine

--- src/process/seekersBase.class.ts
+++ src/process/seekersBase.class.ts
@@ -2,48 +2,45 @@ import { __specialArray__, IterateOnAnArray } from "./specialArray.process";
 
 export class SeekersBase {
   name: string;
-  process: unknown;
-  SpecialArray: Pick<SeekersBaseInterface, "SpecialArray">;
+  process: process;
+  features: Pick<SeekersBaseInterface, "SpecialArray">;
 
   constructor({
     name = "",
-    SpecialArray = {},
-    Props = false,
+    SpecialArray,
     __error,
     __process,
   }: SeekersBaseInterface) {
     this.name = name;
-    this.process = this.#PROCESS(__process);
-    this.SpecialArray = SpecialArray;
+    this.process = (props) => __process(props);
+    this.features = { SpecialArray };
 
     __error();
-
-    if (this.SpecialArray) {
-      this.findTarget = (target) => this.#FIND_TARGET(target);
-    }
   }
 
-  #FIND_TARGET(currentTarget) {
-    const SA = this.SpecialArray;
+  find_target(currentTarget: string) {
+    const SA = this.features.SpecialArray;
 
     // if the options empty, the function will stopped
     if (!(currentTarget != undefined) && !(SA.elementsToGive != undefined))
       return;
 
-    if (!SA.targets.flat().includes(currentTarget)) return undefined;
+    if (typeof SA.targets != "string") {
+      if (!SA.targets.flat().includes(currentTarget)) return undefined;
+    }
+
+    if (typeof SA.targets === "string") {
+      if (SA.targets === currentTarget) return;
+    }
 
     const TARGET_INDEX = IterateOnAnArray(SA.targets, currentTarget);
     const TARGET_RESULT = __specialArray__(SA.elementsToGive, TARGET_INDEX);
 
-    const processFun = SA.process && ((props) => SA.process(props));
+    const processFun = SA.process && ((props: unknown) => SA.process(props));
 
     return {
       result: TARGET_RESULT,
       process: processFun,
     };
-  }
-
-  #PROCESS(customProcess) {
-    return customProcess();
   }
 }
