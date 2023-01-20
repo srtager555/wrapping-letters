0fad09df8dc4749a3c4f2ce784b4df54be2ae2d4
Carlos && Ponce <srtager555@gmail.com>
2023-01-19 22:26:08 -0600

    I moved the specialArray process to the engine

---
+++ src/process/specialArray.procces.ts
@@ -0,0 +1,35 @@
+export function Process__IndexMatchFinder_(
+  array: Array<string[] | string>,
+  word: string
+) {
+  return array
+    .map((element, index) => {
+      if (Array.isArray(element)) {
+        if (
+          element.some((el) => {
+            return el === word;
+          })
+        ) {
+          return index;
+        }
+      }
+
+      if (element === word) return index;
+
+      return -1;
+    })
+    .find((el) => el != -1);
+}
+
+export function Process__SpecialArray_(
+  specialArray: SelectClassElementsToGive | SpecialWrappElementsToGive,
+  SPECIAL_INDEX: number
+) {
+  if (Array.isArray(specialArray)) {
+    if (SPECIAL_INDEX > specialArray.length - 1) {
+      return specialArray[0];
+    } else {
+      return specialArray[SPECIAL_INDEX];
+    }
+  } else return specialArray;
+}
